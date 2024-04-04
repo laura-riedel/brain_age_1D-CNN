1e104c254524dd6fb96d541064944a21283ce6f8
laura-riedel <laura.riedel@posteo.de>
2024-04-04 11:29:31 +0200

    Update define_heldout_test_set.py to include flexible amount of variants to consider at once; define heldout test set for 7/17n 100/200/500p

--- src/prep/define_heldout_test_set.py
+++ src/prep/define_heldout_test_set.py
@@ -2,7 +2,7 @@
 Sample subject IDs randomly to use as held-out final test set and save the list.
 
 Call with e.g.:
-$ python3 define_heldout_test_set.py --k 5000 --seed 43 --name "heldout_test_set.csv"
+$ python3 define_heldout_test_set.py --k 5000 --seed 43 --name "heldout_test_set.csv" --variants '7n100p' '7n200p' '7n500p' '17n100p' '17n200p' '17n500p'
 """
 import pandas as pd
 import random
@@ -12,6 +12,8 @@ from brain_age_prediction import utils
 
 # prepare data path
 schaefer_data_dir = '../../data/schaefer/'
+# baseline variants
+all_variants = ['7n100p','7n200p','7n500p','7n700p','7n1000p','17n100p','17n200p','17n500p','17n700p','17n1000p']
 
 ##################################################################################
 # get parameters from console
@@ -19,9 +21,14 @@ parser = argparse.ArgumentParser(prog='DefineHeldoutTestSet',
                                 description='Sample subject IDs randomly to use as held-out final test set and save the list.',
                                 epilog='Sampling complete, list saved.')
 
-parser.add_argument('--k', type=int, default=5000, help='Enter number of requested ID samples. Default: 5000')
-parser.add_argument('--seed', type=int, default=43, help='Enter random seed to be applied. Default: 43')
-parser.add_argument('--name', type=str, default='heldout_test_set.csv', help='Enter file name to save as. Default: "heldout_test_set.csv"')
+parser.add_argument('--k', type=int, default=5000, 
+                    help='Enter number of requested ID samples. Default: 5000')
+parser.add_argument('--seed', type=int, default=43, 
+                    help='Enter random seed to be applied. Default: 43')
+parser.add_argument('--name', type=str, default='heldout_test_set.csv', 
+                    help='Enter file name to save as. Default: "heldout_test_set.csv"')
+parser.add_argument('--variants', nargs='+', default=all_variants, 
+                    help='Enter variants of interest. Default: "7n100p" "7n200p" "7n500p" "7n700p" "7n1000p" "17n100p" "17n200p" "17n500p" "17n700p" "17n1000p"')
 
 args = parser.parse_args()
 
@@ -29,17 +36,12 @@ args = parser.parse_args()
 # make random sampling reproducible
 utils.make_reproducible(args.seed)
 
-# load availability overview
-schaefer_exists_df = pd.read_csv(schaefer_data_dir+'schaefer_exists.csv')
-# limit to cases of existing, non-empty files
-schaefer_exists_df.drop(schaefer_exists_df[schaefer_exists_df['schaefer_exists'] == False].index, inplace=True)
-schaefer_exists_df.drop(schaefer_exists_df[schaefer_exists_df['is_empty'] == True].index, inplace=True)
-schaefer_exists_df.drop(schaefer_exists_df[schaefer_exists_df['contains_nan'] == True].index, inplace=True)
-# reset index
-schaefer_exists_df = schaefer_exists_df.reset_index(drop=True, inplace=False)
+print('VARIANTS:', args.variants)
+
+assert set(args.variants).intersection(set(all_variants)) == set(args.variants), "There is a mistake in the variable names. Check the spelling!"
 
 # get list of subject IDs
-sub_ids = list(schaefer_exists_df['eid'])     
+sub_ids = utils.get_usable_schaefer_ids(variants=args.variants)
 
 # sample for held-out test set
 test_set = random.sample(sub_ids, k=args.k)
