42bcdc3314977204826d2dc8f56a2f27c3b3aaae
laura-riedel <laura.riedel@posteo.de>
2024-08-05 13:50:12 +0200

    Add shortcut for accessing heldout test set FC matrices

--- src/prep/add_split_shortcuts.py
+++ src/prep/add_split_shortcuts.py
@@ -32,6 +32,8 @@ parser.add_argument('--remove_0', type=bool, default=True,
                     help='Enter boolean flag whether to save additional Xs where the zeros from the matrices are removed. Default: False.')
 parser.add_argument('--datasplit_dir', type=str, default='../../data/schaefer/', 
                     help='Enter path to where the "data_info/" directory is located. This gives information which splits to use (and which IDs to ignore based on the related heldout test set). Default: ../../data/schaefer/')
+parser.add_argument('--heldout_dir', type=str, default='../../data/schaefer/overview_heldout_test_set_100-500p.csv', 
+                    help='Enter path to heldout test set overview csv. Default: ../../data/schaefer/overview_heldout_test_set_100-500p.csv')
 parser.add_argument('--variants', nargs='+', default=variants, 
                     help='Enter variants of interest. Default: "7n100p" "7n200p" "7n500p" "17n100p" "17n200p" "17n500p"')
 
@@ -50,6 +52,8 @@ with h5py.File(save_dir+'schaefer_fc_matrices.hdf5', 'a') as hdf5:
         print('With 0...')
         X_train, y_train, X_val, y_val, X_test, y_test = sklearn_utils.load_dataset(schaefer_variant=variant, remove_0=False, 
                                                                                     flatten=args.flatten, datasplit_dir=args.datasplit_dir)
+        X_heldout, y_heldout = sklearn_utils.load_datasplit(split='heldout_test', schaefer_variant=variant, remove_0=False,
+                                                            flatten=True, datasplit_dir=args.heldout_dir)
         addendum = ''
         if args.flatten:
             addendum = '_flattened'
@@ -59,13 +63,18 @@ with h5py.File(save_dir+'schaefer_fc_matrices.hdf5', 'a') as hdf5:
         grp.create_dataset(variant+'/val/y', data=y_val, compression='gzip', compression_opts=9)
         grp.create_dataset(variant+'/test/X'+addendum, data=X_test, compression='gzip', compression_opts=9)
         grp.create_dataset(variant+'/test/y', data=y_test, compression='gzip', compression_opts=9)
+        grp.create_dataset(variant+'/heldout_test/X'+addendum, data=X_heldout, compression='gzip', compression_opts=9)
+        grp.create_dataset(variant+'/heldout_test/y', data=y_heldout, compression='gzip', compression_opts=9)
         print('Done.')
         if args.remove_0:
             print('Without 0...')
             X_train, _, X_val, _, X_test, _ = sklearn_utils.load_dataset(schaefer_variant=variant, remove_0=True, 
                                                                          flatten=True, datasplit_dir=args.datasplit_dir)
+            X_heldout, _ = sklearn_utils.load_datasplit(split='heldout_test', schaefer_variant=variant, remove_0=True,
+                                                        flatten=True, datasplit_dir=args.heldout_dir)
             grp.create_dataset(variant+'/train/X_no-0', data=X_train, compression='gzip', compression_opts=9)
             grp.create_dataset(variant+'/val/X_no-0', data=X_val, compression='gzip', compression_opts=9)
             grp.create_dataset(variant+'/test/X_no-0', data=X_test, compression='gzip', compression_opts=9)
+            grp.create_dataset(variant+'/heldout_test/X_no-0', data=X_heldout, compression='gzip', compression_opts=9)
             print('Done.')
         
